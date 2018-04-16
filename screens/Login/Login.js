3e2dc0166adf24ece40412bc9dc97ef08b11697f
Taylor Sam <taylorsam209@yahoo.com>
2018-04-15 22:52:04 -0600

    first commit

---
+++ screens/Login/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "../styles";
+import { Text } from "react-native";
+
+const Login = props => {
+  return (
+    <Container>
+      <Text>Login</Text>
+    </Container>
+  );
+};
+
+export default Login;
